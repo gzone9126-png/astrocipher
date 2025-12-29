bad03186135ef404a438f2c3b1b067ce65ebbacb
gzone9126-png <gzone9126@gmail.com>
2025-12-29 12:17:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function openBooking(serviceName) {
     document.getElementById("booking").scrollIntoView({ behavior: "smooth" });
     document.getElementById("service").value = serviceName;
 }
+function toggleQR() {
+    let qr = document.getElementById("qrBox");
+
+    if (qr.style.display === "none") {
+        qr.style.display = "block";
+    } else {
+        qr.style.display = "none";
+    }
+}
