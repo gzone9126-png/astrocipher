82fa9d23b47c0394bf54d258edc18fdd77e9bb85
gzone9126-png <gzone9126@gmail.com>
2025-12-30 10:05:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,10 @@ function toggleQR() {
         qr.style.display = "none";
     }
 }
+function openAbout() {
+    document.getElementById("aboutModal").style.display = "block";
+}
+
+function closeAbout() {
+    document.getElementById("aboutModal").style.display = "none";
+}
